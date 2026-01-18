0659466a821f187e464b0e81195c3aae5301c7ee
sheshiyer <sheshnarayan.iyer@gmail.com>
2026-01-18 22:52:27 +0530

    feat: refactor text styling with design tokens

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,11 +14,24 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        serif: ['"Cormorant Garamond"', 'serif'],
-        sans: ['"Lato"', 'sans-serif'],
-        body: ['"Crimson Text"', 'serif'],
-        earthy: ['"Tex Gyre Termes"', 'serif'],
-        munchy: ['"Solway"', 'sans-serif'],
+        heading: ['"Tex Gyre Termes"', 'serif'],
+        subheading: ['"Solway"', 'sans-serif'],
+        body: ['"Lato"', 'sans-serif'],
+      },
+      fontSize: {
+        'xs': '12px',
+        'sm': '14px',
+        'base': '16px',
+        'lg': '18px',
+        'xl': '20px',
+        '2xl': '24px',
+        '3xl': '30px',
+        '4xl': '36px',
+        '5xl': '48px',
+        '6xl': '60px',
+        '7xl': '72px',
+        '8xl': '96px',
+        '9xl': '128px',
       },
       colors: {
         earth: {
